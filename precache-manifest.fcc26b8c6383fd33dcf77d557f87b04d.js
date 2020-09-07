aec81e5ddb4bc6dde6af9d082b669083c43ba2f2
Ian Lim <ianlim@Ians-MacBook-Pro.local>
2020-09-06 21:14:11 -0500

    Updates

---
+++ precache-manifest.fcc26b8c6383fd33dcf77d557f87b04d.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ebe91acb098acb6960e262e1c51cba",
+    "url": "/seam.com/index.html"
+  },
+  {
+    "revision": "0798d08767b649f8099b",
+    "url": "/seam.com/static/css/2.3a395901.chunk.css"
+  },
+  {
+    "revision": "e1acfa6410f0e9950289",
+    "url": "/seam.com/static/css/main.00770f9f.chunk.css"
+  },
+  {
+    "revision": "0798d08767b649f8099b",
+    "url": "/seam.com/static/js/2.1cd8c5cf.chunk.js"
+  },
+  {
+    "revision": "0fd07708345db41e7462430257b87932",
+    "url": "/seam.com/static/js/2.1cd8c5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1acfa6410f0e9950289",
+    "url": "/seam.com/static/js/main.a24b559b.chunk.js"
+  },
+  {
+    "revision": "8b6c041255f62ea0cde3",
+    "url": "/seam.com/static/js/runtime-main.b7dbbe18.js"
+  },
+  {
+    "revision": "f4249c9a9bef6f1fa3c971cc6e978937",
+    "url": "/seam.com/static/media/appDemo.f4249c9a.png"
+  },
+  {
+    "revision": "348807c7ecb7270d15fccff16ffe3fe4",
+    "url": "/seam.com/static/media/arrow-right-icon.348807c7.svg"
+  },
+  {
+    "revision": "c66b213d1dfff391c61146727032f068",
+    "url": "/seam.com/static/media/chevron-down.c66b213d.svg"
+  },
+  {
+    "revision": "5f12b0dc993b9ea0bb9c928d98f89c07",
+    "url": "/seam.com/static/media/customers-logo-strip.5f12b0dc.png"
+  },
+  {
+    "revision": "7c1a6f97bf87de69a244342e6c17389c",
+    "url": "/seam.com/static/media/customize-icon.7c1a6f97.svg"
+  },
+  {
+    "revision": "ee1c0c55aa99cd9524551ef19b86aad3",
+    "url": "/seam.com/static/media/design-illustration-2.ee1c0c55.svg"
+  },
+  {
+    "revision": "b3eaa996331ef9f0e9ac1327678c6b67",
+    "url": "/seam.com/static/media/distribution-icon.b3eaa996.svg"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/seam.com/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "a1ad24916d979e088c5e8c80b073f92b",
+    "url": "/seam.com/static/media/email-illustration.a1ad2491.svg"
+  },
+  {
+    "revision": "7064f155dc1611550f5ade904f1964e3",
+    "url": "/seam.com/static/media/email-newsletter-icon.7064f155.svg"
+  },
+  {
+    "revision": "6c9e80eec6977f6497b0e55f0fce374a",
+    "url": "/seam.com/static/media/fast-icon.6c9e80ee.svg"
+  },
+  {
+    "revision": "61eae8b2845b7d35c3d58786733ea3d2",
+    "url": "/seam.com/static/media/github-icon.61eae8b2.svg"
+  },
+  {
+    "revision": "428208e0c0a45141a4a5b6871cf32cb4",
+    "url": "/seam.com/static/media/group-icon.428208e0.svg"
+  },
+  {
+    "revision": "9f39623921171360f5e99c3693f265c1",
+    "url": "/seam.com/static/media/ianHeadshot.9f396239.png"
+  },
+  {
+    "revision": "fa5666fa9730a9f927a76edfa9b53e2b",
+    "url": "/seam.com/static/media/linkedin-icon.fa5666fa.svg"
+  },
+  {
+    "revision": "df5ff1de5c6e7c21fb170cce8b3c170b",
+    "url": "/seam.com/static/media/live-stream-icon.df5ff1de.svg"
+  },
+  {
+    "revision": "f9776f55173cd40b2b3f3fed1c7443c4",
+    "url": "/seam.com/static/media/logo.f9776f55.svg"
+  },
+  {
+    "revision": "a47dacaf9ce8a8cb92f54b8bff5febe5",
+    "url": "/seam.com/static/media/maxHeadshot.a47dacaf.jpeg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/seam.com/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "a12572bdffec3f4a0b1df5e8a332a139",
+    "url": "/seam.com/static/media/reliable-icon.a12572bd.svg"
+  },
+  {
+    "revision": "297d50a2cd8b864f0c1f357e5108d87a",
+    "url": "/seam.com/static/media/shield-icon.297d50a2.svg"
+  },
+  {
+    "revision": "190694ce5c2e88862177e6622e551e3c",
+    "url": "/seam.com/static/media/shopping-bag-icon.190694ce.svg"
+  },
+  {
+    "revision": "0dd41402bf25d6daea592bae22097bf3",
+    "url": "/seam.com/static/media/shopping-cart-icon.0dd41402.svg"
+  },
+  {
+    "revision": "345871b443e4f5f841bedccf11a61859",
+    "url": "/seam.com/static/media/simple-icon.345871b4.svg"
+  },
+  {
+    "revision": "ea86ae7e7e40914332a5e521c867c79f",
+    "url": "/seam.com/static/media/star-icon.ea86ae7e.svg"
+  },
+  {
+    "revision": "135d403ccd52c50bf2e773a8a68cea57",
+    "url": "/seam.com/static/media/support-icon.135d403c.svg"
+  },
+  {
+    "revision": "ec52da39f670db5b4b3c32f54914a919",
+    "url": "/seam.com/static/media/svg-decorator-blob-1.ec52da39.svg"
+  },
+  {
+    "revision": "159be2476eedd8b63bb0c7e0a58d0e47",
+    "url": "/seam.com/static/media/svg-decorator-blob-3.159be247.svg"
+  },
+  {
+    "revision": "c86fb21a2396dc9a8149429f1cd451cf",
+    "url": "/seam.com/static/media/svg-decorator-blob-7.c86fb21a.svg"
+  },
+  {
+    "revision": "19df9c1d30e1ee23905d4034a5eb5da3",
+    "url": "/seam.com/static/media/svg-decorator-blob-8.19df9c1d.svg"
+  },
+  {
+    "revision": "006c03805c9d689b561825957bbfd354",
+    "url": "/seam.com/static/media/tag-icon.006c0380.svg"
+  },
+  {
+    "revision": "5ae3fe7a0d10e5c14ec0575db276dcda",
+    "url": "/seam.com/static/media/team-illustration-2.5ae3fe7a.svg"
+  },
+  {
+    "revision": "7189052ca672d113f057d9a4f3cb5efe",
+    "url": "/seam.com/static/media/twitter-icon.7189052c.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/seam.com/static/media/x.b0200bf7.svg"
+  }
+]);
